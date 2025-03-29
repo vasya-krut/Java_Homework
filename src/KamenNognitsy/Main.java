e6eda95f7f7591a65f192d7a6b4a6875af9d5365
vasya-krut <flopergthd@gmail.com>
2025-03-29 18:19:58 +0500

    Код программ для модуля 7 обновлен

--- src/KamenNognitsy/Main.java
+++ src/KamenNognitsy/Main.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Игрок 1");
-        Player player1 = new Player();
-        System.out.println("Игрок 2");
-        Player player2 = new Player();
-        Player.printNumRounds();
-        while(Player.numCurRound < Player.numRounds)
-        {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.println("Игрок 1, введите свое имя");
+        name = scanner.next();
+        Player player1 = new Player(name);
+        System.out.println("Игрок 2, введите свое имя");
+        name = scanner.next();
+        Player player2 = new Player(name);
+
+
+        int numRounds = 2; //количество раундов в игре
+        int numCurRound = 0; // номер текущего руанда
+        int curRoundWinner = 0;//0, если ничья: 1, если победил 1 игрок; 2, если победил 2 игрок
+
+        System.out.println("Игра ведется до " + numRounds + " побед");
+        while(numCurRound < numRounds){
             player1.cardChoose();
             player2.cardChoose();
-            Player.playRound(player1.card, player2.card);
-            if (Player.curRoundWinner == 1 | Player.curRoundWinner == 2)
+            curRoundWinner = Player.playRound(player1.card, player2.card);
+            if (curRoundWinner == 1 | curRoundWinner == 2)
             {
-                if (Player.curRoundWinner == 1)
+                if (curRoundWinner == 1)
                     player1.winner();
                 else
                     player2.winner();
-                Player.numCurRound++;
+                numCurRound++;
             }
             else
                 System.out.println("Ничья! Раунд переигрывается.");
@@ -33,15 +42,11 @@ public class Main {
         String name = "Player";
         int card = 0;// код одной выбранной карты из четырех
         int numWinner = 0; //количество побед игрока
-        static int numRounds = 2; //количество раундов в игре
-        static int numCurRound = 0; // номер текущего руанда
-        static int curRoundWinner = 0;//0, если ничья: 1, если победил 1 игрок; 2, если победил 2 игрок
 
-        Player()
+
+        Player(String name)
         {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите свое имя ...");
-            name = scanner.next();
+            this.name = name;
         }
 
         public void cardChoose()
@@ -51,60 +56,53 @@ public class Main {
             {
                 System.out.println("Игрок " + name +", выберите одну из карт: 0 - камень, 1 - ножницы, 2 - бумага, 3 - колодец");
                 card = scanner.nextInt();
-                if (card<0 | card>3)
+                if (card < 0 | card > 3) {
                     System.out.println("Введена неверная цифра! Введите еще раз");
-                else break;
+                } else break;
             }
         }
 
-        static void printNumRounds()
-        {
-            System.out.println("Игра ведется до " + numRounds + " побед");
-        }
+
 
         public void getNumWinner()
         {
             System.out.println("Игрок " + name + " одержал " + numWinner + " побед");
         }
 
-        static void playRound(int player1Card, int player2Card)
+        static int playRound(int player1Card, int player2Card)
         {
             if(player1Card == player2Card)
             {
-                curRoundWinner = 0;
-                return;
+                return 0;
             }
             switch (player1Card)
             {
                 case 0:{
                     if (player2Card == 1)
-                        curRoundWinner = 1;
+                        return  1;
                     else
-                        curRoundWinner = 2;
-                    break;
+                        return  2;
                 }
                 case 1:{
                     if (player2Card == 2)
-                        curRoundWinner = 1;
+                        return  1;
                     else
-                        curRoundWinner = 2;
-                    break;
+                        return  2;
                 }
                 case 2:{
                     if (player2Card == 0 | player2Card == 3)
-                        curRoundWinner = 1;
+                        return  1;
                     else
-                        curRoundWinner = 2;
-                    break;
+                        return  2;
                 }
                 case 3:{
                     if (player2Card == 0 | player2Card == 1)
-                        curRoundWinner = 1;
+                        return  1;
                     else
-                        curRoundWinner = 2;
-                    break;
+                        return  2;
                 }
             }
+            return 0;//никогда не выполнится
         }
 
         public void winner()
--- src/Viselitsa/Main.java
+++ src/Viselitsa/Main.java
@@ -1,40 +1,75 @@
 package Viselitsa;
 
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Первый игрок, введите свое имя");
+        String name1 = scanner.next();
+        Player player1 = new Player(name1);
+        System.out.println("Второй игрок, введите свое имя");
+        String name2 = scanner.next();
+        Player player2 = new Player(name2);
+        System.out.println("Введите количество побед, до которого идет игра");
+        int numMaxWins = scanner.nextInt();
+        int gameNum = 1; // номер игры
 
-        while(!Player.viselitsa())
-        {
-            System.out.println("До выигрыша первого игрока осталось " + (Player.numMaxRounds - Player.numLooseRound) + " раундов");
-            player.printUgadal();
-            Player.playRound();
-            if(Player.ugadalSlovo()) break;
+        while(player1.numWins < numMaxWins & player2.numWins < numMaxWins) {
+            if(gameNum % 2 == 1){//каждую игру игроки загадывающий и отгадывающий слово меняются местами
+                name1 = player1.name;
+                name2 = player2.name;
+            }
+            else {
+                name2 = player1.name;
+                name1 = player2.name;
+            }
+
+            Game game = new Game(name1, name2);
+
+            if (game.playGame()){
+                System.out.println(name2 + " выиграл");
+                if(gameNum % 2 == 1)
+                    player2.numWins++;
+                else
+                    player1.numWins++;
+            }
+            else {
+                System.out.println(name1 + " выиграл");
+                if(gameNum % 2 == 1)
+                    player1.numWins++;
+                else
+                    player2.numWins++;
+            }
+            System.out.println("Счет после " + gameNum + " раунда стал: " + player1.name + " " + player1.numWins + ", " + player2.name + " " + player2.numWins);
+            gameNum++;
         }
-        if (!Player.viselitsa())
-            System.out.println("Выиграл второй игрок");
+        if(player1.numWins > player2.numWins)
+            System.out.println("Серию игр выиграл " + player1.name);
         else
-            System.out.println("Выиграл первый игрок");
-
+            System.out.println("Серию игр выиграл " + player2.name);
     }
 
-    static class Player{
-        static String ugadika;//загадавыаемое слово
-        static String ugadal;
-        static int numLooseRound = 0;//число неудачных попыток
-        static int numMaxRounds = "Виселица".length();// максимальное количество раундов
+    static class Game{
+        String player1Name;
+        String player2Name;
+        private String ugadika;//загадавыаемое слово
+        private String ugadal;
+        private int numLooseRound = 0;//число неудачных попыток
+        final int numMaxRounds = "Виселица".length();// максимальное количество раундов
 
-        Player()
+
+        Game(String name1, String name2)
         {
+            player1Name = name1;//всегда загадывает слово
+            player2Name = name2;// всегда отгадывает слово
             Scanner scanner = new Scanner(System.in);
             boolean flag = true;
             while(flag)
             {
                 flag = false;
-                System.out.println("Первый игрок, загадайте слово");
+                System.out.println(player1Name + ", загадайте слово");
                 ugadika = scanner.next().toLowerCase();
                 char[] chars = ugadika.toCharArray();
                 for (char c : chars)
@@ -51,16 +86,27 @@ public class Main {
             ugadal = str.toString();
         }
 
-       static void printUgadal()
+        public boolean playGame(){//возвращает true, если победил второй игрок (отгадывающий слово)
+
+            while (!viselitsa()) {
+                System.out.println("До выигрыша игрока " + player1Name + " осталось " + (numMaxRounds - numLooseRound) + " раундов");
+                printUgadal();
+                playRound();
+                if (ugadalSlovo()) break;
+            }
+            return ugadalSlovo();
+        }
+
+       private void printUgadal()
         {
-            System.out.println("Все, что знает второй игрок о загаднном слове");
+            System.out.println("Все, что знает " + player2Name + " о загаднном слове");
             System.out.println(ugadal);
         }
 
-        static void playRound()
+        private void playRound()
         {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Второй игрок, введите букву");
+            System.out.println( player2Name + ", введите букву");
             char letter = scanner.next().charAt(0);
             if(!(Character.isLetter(letter)))
             {
@@ -70,7 +116,7 @@ public class Main {
             if(ugadika.indexOf(letter) == -1)
             {
                 numLooseRound++;
-                System.out.println("Второй игрок не угадал, такой буквы в слове нет");
+                System.out.println(player2Name + " не угадал, такой буквы в слове нет");
                 return;
             }
             int indexStart = 0;
@@ -83,22 +129,34 @@ public class Main {
                 ugadal = str.toString();
                 indexStart++;
             }
-            System.out.println("Второй игрок угадал, такая буква в слове есть");
+            System.out.println(player2Name + " угадал, такая буква в слове есть");
         }
 
-        static boolean ugadalSlovo()
+        private boolean ugadalSlovo()
         {
             if (ugadal.indexOf('*') == -1)
                 return true;
             return false;
         }
 
-        static boolean viselitsa()
+        private boolean viselitsa()
         {
            if (numLooseRound < numMaxRounds)
                return false;
-
            return true;
         }
+    }
+
+    static class Player{
+        String name;
+        int numWins = 0;
+        Player(String name){
+            this.name = name;
+        }
+
+        public void playerWin(){
+            numWins++;
+        }
+
     }
 }
